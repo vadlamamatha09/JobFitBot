299aee700e353ec668d6ea5874eaf6fa13643d1a
vadlamamatha09 <vadlamamatha09@gmail.com>
2026-03-10 21:37:29 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+
+import streamlit as st
+import pickle
+
+model = pickle.load(open("jobfit_model.pkl","rb"))
+
+st.title("JobFitBot - Job Eligibility Predictor")
+
+education = st.selectbox("Education", ["BTech","MBA","BSc","MTech","BCA"])
+skills = st.text_input("Enter your skills")
+certification = st.selectbox("Certification",["yes","no"])
+experience = st.slider("Years of Experience",0,5)
+
+if st.button("Predict Job"):
+
+    edu = 1
+    skill = 1
+    cert = 1 if certification=="yes" else 0
+
+    prediction = model.predict([[edu,skill,cert,experience]])
+
+    st.success("You are eligible for a suitable job role!")
